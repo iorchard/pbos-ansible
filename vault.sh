186f659e21c6cc60642394f31f6e0655d2ddb422
Heechul Kim <jijisa@iorchard.co.kr>
2021-05-26 18:30:58 +0900

    add keepalived and memcached

--- vault.sh
+++ vault.sh
@@ -7,11 +7,13 @@ VAULTFILE="inventory/${MYSITE}/group_vars/all/vault.yml"
 read -s -p 'ssh password: ' SSHPASS; echo ""
 read -s -p 'sudo password: ' SUDOPASS; echo ""
 MARIADBROOTPASS=$(head /dev/urandom |tr -dc A-Za-z0-9 |head -c 8)
+RABBITMQPASS=$(head /dev/urandom |tr -dc A-Za-z0-9 |head -c 8)
 
 echo "---" > $VAULTFILE
 echo "vault_ssh_password: '$SSHPASS'" >> $VAULTFILE
 echo "vault_sudo_password: '$SUDOPASS'" >> $VAULTFILE
 echo "vault_mariadb_root_password: '$MARIADBROOTPASS'" >> $VAULTFILE
+echo "vault_rabbitmq_openstack_password: '$RABBITMQPASS'" >> $VAULTFILE
 echo -n "..." >> $VAULTFILE
 head /dev/urandom |tr -dc A-Za-z0-9 |head -c 8 > .vaultpass
 ansible-vault encrypt $VAULTFILE

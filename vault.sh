116b395ede76ee142e59847413e2862d6edc7182
Heechul Kim <jijisa@iorchard.co.kr>
2021-07-05 18:55:55 +0900

    add neutron/nova/horizon

--- vault.sh
+++ vault.sh
@@ -9,9 +9,14 @@ read -s -p 'sudo password: ' SUDOPASS; echo ""
 read -s -p 'openstack admin password: ' OS_ADMIN_PASS; echo ""
 MARIADB_ROOTP_ASS=$(head /dev/urandom |tr -dc A-Za-z0-9 |head -c 8)
 RABBITMQ_PASS=$(head /dev/urandom |tr -dc A-Za-z0-9 |head -c 8)
-MARIADB_KEYSTONE_PASS=$(head /dev/urandom |tr -dc A-Za-z0-9 |head -c 8)
+KEYSTONE_PASS=$(head /dev/urandom |tr -dc A-Za-z0-9 |head -c 8)
 GLANCE_PASS=$(head /dev/urandom |tr -dc A-Za-z0-9 |head -c 8)
 PLACEMENT_PASS=$(head /dev/urandom |tr -dc A-Za-z0-9 |head -c 8)
+NEUTRON_PASS=$(head /dev/urandom |tr -dc A-Za-z0-9 |head -c 8)
+METADATA_SECRET=$(head /dev/urandom |tr -dc A-Za-z0-9 |head -c 8)
+CINDER_PASS=$(head /dev/urandom |tr -dc A-Za-z0-9 |head -c 8)
+RBD_SECRET=$(cat /proc/sys/kernel/random/uuid)
+NOVA_PASS=$(head /dev/urandom |tr -dc A-Za-z0-9 |head -c 8)
 
 echo "---" > $VAULTFILE
 echo "vault_ssh_password: '$SSHPASS'" >> $VAULTFILE
@@ -19,9 +24,14 @@ echo "vault_sudo_password: '$SUDOPASS'" >> $VAULTFILE
 echo "vault_openstack_admin_password: '$OS_ADMIN_PASS'" >> $VAULTFILE
 echo "vault_mariadb_root_password: '$MARIADB_ROOT_PASS'" >> $VAULTFILE
 echo "vault_rabbitmq_openstack_password: '$RABBITMQ_PASS'" >> $VAULTFILE
-echo "vault_mariadb_keystone_password: '$MARIADB_KEYSTONE_PASS'" >> $VAULTFILE
+echo "vault_keystone_password: '$KEYSTONE_PASS'" >> $VAULTFILE
 echo "vault_glance_password: '$GLANCE_PASS'" >> $VAULTFILE
 echo "vault_placement_password: '$PLACEMENT_PASS'" >> $VAULTFILE
+echo "vault_neutron_password: '$NEUTRON_PASS'" >> $VAULTFILE
+echo "vault_metadata_secret: '$METADATA_SECRET'" >> $VAULTFILE
+echo "vault_cinder_password: '$CINDER_PASS'" >> $VAULTFILE
+echo "vault_rbd_secret: '$RBD_SECRET'" >> $VAULTFILE
+echo "vault_nova_password: '$NOVA_PASS'" >> $VAULTFILE
 echo -n "..." >> $VAULTFILE
 head /dev/urandom |tr -dc A-Za-z0-9 |head -c 8 > .vaultpass
 ansible-vault encrypt $VAULTFILE

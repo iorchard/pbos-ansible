2b963f6d5d96baef3a58616b7336106787c85273
Heechul Kim <jijisa@iorchard.co.kr>
2021-08-13 11:57:53 +0900

    changed to prompt user password; no need to ask twice for ssh and sudo password since it is the same

--- vault.sh
+++ vault.sh
@@ -3,8 +3,7 @@
 VAULTFILE="inventory/${MYSITE}/group_vars/all/vault.yml"
 
 # Create vault file.
-read -s -p 'ssh password: ' SSHPASS; echo ""
-read -s -p 'sudo password: ' SUDOPASS; echo ""
+read -s -p "$USER password: " USERPASS; echo ""
 read -s -p 'openstack admin password: ' OS_ADMIN_PASS; echo ""
 MARIADB_ROOT_PASS=$(head /dev/urandom |tr -dc A-Za-z0-9 |head -c 8)
 RABBITMQ_PASS=$(head /dev/urandom |tr -dc A-Za-z0-9 |head -c 8)
@@ -21,8 +20,8 @@ BARBICAN_PASS=$(head /dev/urandom |tr -dc A-Za-z0-9 |head -c 8)
 BARBICAN_KEK=$(head /dev/urandom |tr -dc A-Za-z0-9 |head -c 32)
 
 echo "---" > $VAULTFILE
-echo "vault_ssh_password: '$SSHPASS'" >> $VAULTFILE
-echo "vault_sudo_password: '$SUDOPASS'" >> $VAULTFILE
+echo "vault_ssh_password: '$USERPASS'" >> $VAULTFILE
+echo "vault_sudo_password: '$USERPASS'" >> $VAULTFILE
 echo "vault_openstack_admin_password: '$OS_ADMIN_PASS'" >> $VAULTFILE
 echo "vault_mariadb_root_password: '$MARIADB_ROOT_PASS'" >> $VAULTFILE
 echo "vault_rabbitmq_openstack_password: '$RABBITMQ_PASS'" >> $VAULTFILE

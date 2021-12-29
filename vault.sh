0ba8d86b887ff24d510d2dd64cd14b0c0815b0ea
Heechul Kim <jijisa@iorchard.co.kr>
2021-12-29 12:01:43 +0900

    add ca_passphrase in vault

--- vault.sh
+++ vault.sh
@@ -5,6 +5,7 @@ VAULTFILE="inventory/${MYSITE}/group_vars/all/vault.yml"
 # Create vault file.
 read -s -p "$USER password: " USERPASS; echo ""
 read -s -p 'openstack admin password: ' OS_ADMIN_PASS; echo ""
+CA_PASS=$(head /dev/urandom |tr -dc A-Za-z0-9 |head -c 8)
 MARIADB_ROOT_PASS=$(head /dev/urandom |tr -dc A-Za-z0-9 |head -c 8)
 RABBITMQ_PASS=$(head /dev/urandom |tr -dc A-Za-z0-9 |head -c 8)
 KEYSTONE_PASS=$(head /dev/urandom |tr -dc A-Za-z0-9 |head -c 8)
@@ -23,6 +24,7 @@ echo "---" > $VAULTFILE
 echo "vault_ssh_password: '$USERPASS'" >> $VAULTFILE
 echo "vault_sudo_password: '$USERPASS'" >> $VAULTFILE
 echo "vault_openstack_admin_password: '$OS_ADMIN_PASS'" >> $VAULTFILE
+echo "vault_ca_passphrase: '$CA_PASS'" >> $VAULTFILE
 echo "vault_mariadb_root_password: '$MARIADB_ROOT_PASS'" >> $VAULTFILE
 echo "vault_rabbitmq_openstack_password: '$RABBITMQ_PASS'" >> $VAULTFILE
 echo "vault_keystone_password: '$KEYSTONE_PASS'" >> $VAULTFILE
